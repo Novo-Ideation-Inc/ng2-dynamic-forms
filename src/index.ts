1e4850a5602602ea60a6716c84063317a2573341
Jamie Bertram <jamie@novoideation.ca>
2016-09-16 17:59:01 -0600

    Export question classes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@
  * Created by Jamie on 2016-09-16.
  */
 /// <reference path="../typings/index.d.ts" />
-export { DynamicFormsModule } from "./dynamic-forms/dynamic-forms.module";
-export { DfValidators } from "./dynamic-forms/df-validators";
-export { DynamicFormComponent } from "./dynamic-forms/dynamic-form.component";
+export { DynamicFormsModule } from './dynamic-forms/dynamic-forms.module';
+export { DfValidators } from './dynamic-forms/df-validators';
+export { DynamicFormComponent } from './dynamic-forms/dynamic-form.component';
+export { QuestionBase } from './dynamic-forms/question-base'
+export { TextfieldQuestion} from './dynamic-forms/question-textfield'
